7771429612d4ce8f5cd42f97f526e27fbb950e80
Ankita Sil <117772860+Sil-99@users.noreply.github.com>
2023-03-16 11:11:38 +0530

    Add files via upload

---
+++ Hostel Pages/script.js
@@ -0,0 +1,17 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+} 
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
